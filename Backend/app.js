fe018b2f7739b91eba1b25879e2cc4c6ad336e01
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-06-24 12:24:07 +0200

    feat : Create an express app pt2

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json( { message : 'Your request was successful' });
+//log "request recieved" & hands on execution
+app.use((req, res, next) => {
+    console.log('Request recieved!');
+    next();
+})
+
+//add 201 status code to the response & hands on execution
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+//send json response & hands on execution
+app.use((req, res, next) => {
+    res.json( { message : 'Your request was successful!' });
+    next()
+})
+
+//log "response sent successfully" to the console
+app.use((req, res, next) => {
+    console.log('Response sent successfully!')
 })
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,51 @@
 const http = require('http');
 const app = require('./app');
 
-app.set('port', process.env.PORT || 3000)
+//return a valid port, whether it is provided as a number or a string
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+const port = normalizePort(process.env.PORT ||'3000'); //FIXME: "·" was between the vertical lines and '3000' but was causing error
+app.set('port', port);
+
+//check for various errors and handle them appropriately — it is then registered to the server
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+//logs the port or named pipe on which the server is running to the console
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
